3574caf3faccc1ed21ecbcb0b3ccb25d5493b767
Meet9292 <21cs023@charusat.edu.in>
2024-06-02 15:54:43 +0530

    intro page done

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <div>About</div>
+
+        <div>
+        <img
+          className="rounded-full shadow-lg w-fit"
+          src="src/assets/1.jpg"
+          alt=""
+        />
+        </div>
+
+        <div></div>
+    </div>
+  )
+}
+
+export default About
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,22 +1,64 @@
-import React from 'react'
+import React from "react";
 
 function Intro() {
   return (
-    <div className='flex items-center'>
-        <div className='w-full border flex justify-center'>
-            <div className='w-2/3'>
-            <h3 className='text-3xl font-semibold'>Hi, I am</h3>
-            <h1 className='text-5xl font-bold'>Meet Kothari</h1>
-            <h2 className='text-2xl'>I am Developer</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum nihil rerum culpa saepe quae optio! Tempora dolores excepturi saepe fugit.</p>
-            <a href="/contact">Contact me</a>
-            </div>
+    <div
+      style={{
+        backgroundImage: `url(src/assets/banner_wallpaper.svg)`,
+        backgroundSize: "cover",
+      }}
+      className="flex items-center"
+    >
+      <div className="w-full flex justify-center text-white">
+        <div className="w-2/3 ms-10">
+          <h3 className="text-3xl font-semibold">Hi, I am</h3>
+          <h1 className=" mt-3 text-5xl font-bold">Meet Kothari</h1>
+          <h2 className="mt-3 text-2xl">I am Developer</h2>
+          <p className="mt-3">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            nihil rerum culpa saepe quae optio! Tempora dolores excepturi saepe
+            fugit.
+          </p>
+          <div className="mt-4 flex gap-5 text-4xl">
+            <a className="border hover:bg-orange-500 cursor-pointer px-3 py-4 w-16 h-16 rounded-full flex justify-center items-center bg-gray-800">
+            <i class="fa-solid fa-code"></i>
+            </a>
+
+            <a className="border hover:bg-orange-500 cursor-pointer px-3 py-4 w-16 h-16 rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+
+            <a className="border hover:bg-orange-500 cursor-pointer px-3 py-4 w-16 h-16 rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+
+            <a className="border hover:bg-orange-500 cursor-pointer px-3 py-4 w-16 h-16 rounded-full flex justify-center items-center bg-gray-800">
+              <i class="fa-brands fa-github"></i>
+            </a>
+          </div>
+          <br />
+          <br />
+          <a
+            className="text-2xl px-4 py-3 bg-orange-500 rounded-full shadow"
+            href="/contact"
+          >
+            Contact me
+          </a>
         </div>
-        <div className='w-full border flex justify-center'>
-            <img className='rounded-full shadow-lg w-fit' src="src/assets/1.jpg" alt="" />
-        </div>
+      </div>
+      <div className="w-full flex justify-center">
+        <img
+          className="rounded-full shadow-lg w-fit"
+          src="src/assets/1.jpg"
+          alt=""
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
+
+
+
+
